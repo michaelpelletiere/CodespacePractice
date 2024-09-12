835caf5cca4846b0409c523320d01a6e8f66f16b
Michael Pelletiere <michael.pelletiere.lab@outlook.com>
2024-09-12 22:46:31 +0000

    Updated About Me

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "gold and white background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Microsoft Software and Systems Academy student. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
